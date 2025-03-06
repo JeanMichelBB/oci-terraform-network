ead20781802c3ac74654b75673acea59e52a12ee
Jean-Michel Bérubé <107640908+JeanMichelBB@users.noreply.github.com>
2025-03-06 14:24:33 -0500

    feat/oci network (#6)
    * feat: add oci network
    
    * feat: fix terraform provider

--- security_group.tf
+++ security_group.tf
@@ -1,75 +1,26 @@
 # // security_group.tf
-
-resource "oci_core_security_list" "public_security_list" {
+resource "oci_core_network_security_group" "db_sg" {
   compartment_id = var.compartment_id
-  vcn_id         = oci_core_virtual_network.vcn.id
-  display_name   = "public_security_list"
+  vcn_id         = oci_core_vcn.main.id
+  display_name   = "Database Security Group"
+}
 
-  ingress_security_rules {
-    protocol = "6" # TCP
-    source   = "0.0.0.0/0"
-    tcp_options {
-      min = 80
-      max = 80
+resource "oci_core_network_security_group" "private_sg" {
+  compartment_id = var.compartment_id
+  vcn_id         = oci_core_vcn.main.id
+  display_name   = "Private Security Group"
+}
+
+resource "oci_core_network_security_group_security_rule" "allow_db_private" {
+  network_security_group_id = oci_core_network_security_group.db_sg.id
+  direction                = "INGRESS"
+  protocol                 = "6"  # TCP
+  source_type              = "NETWORK_SECURITY_GROUP"
+  source                   = oci_core_network_security_group.private_sg.id
+  tcp_options {
+    destination_port_range {
+      min = 1521
+      max = 1521
     }
   }
-
-  egress_security_rules {
-    protocol = "all"
-    destination = "0.0.0.0/0"
-  }
-}
-
-resource "oci_core_security_list" "private_security_list" {
-  compartment_id = var.compartment_id
-  vcn_id         = oci_core_virtual_network.vcn.id
-  display_name   = "private_security_list"
-
-  ingress_security_rules {
-    protocol = "6" # TCP
-    source   = "0.0.0.0/0"
-    tcp_options {
-      min = 22
-      max = 22
-    }
-  }
-
-  egress_security_rules {
-    protocol = "all"
-    destination = "0.0.0.0/0"
-  }
-}
-resource "oci_core_network_security_group" "web_nsg" {
-  compartment_id = var.compartment_id
-  vcn_id         = oci_core_virtual_network.vcn.id
-  display_name   = "web_nsg"
-}
-
-resource "oci_core_network_security_group" "app_nsg" {
-  compartment_id = var.compartment_id
-  vcn_id         = oci_core_virtual_network.vcn.id
-  display_name   = "app_nsg"
-}
-
-# Attach NSG to compute instances
-resource "oci_core_instance" "web_instance" {
-  compartment_id = var.compartment_id
-  availability_domain = var.azs[0]
-  shape            = "VM.Standard2.1"
-  display_name     = "web_instance"
-  image_id         = var.image_id
-  subnet_id        = oci_core_subnet.public_subnet[0].id
-
-  network_security_group_ids = [oci_core_network_security_group.web_nsg.id]
-}
-
-resource "oci_core_instance" "app_instance" {
-  compartment_id = var.compartment_id
-  availability_domain = var.azs[1]
-  shape            = "VM.Standard2.1"
-  display_name     = "app_instance"
-  image_id         = var.image_id
-  subnet_id        = oci_core_subnet.private_subnet[0].id
-
-  network_security_group_ids = [oci_core_network_security_group.app_nsg.id]
 }
--- variables.tf
+++ variables.tf
@@ -1,22 +1,52 @@
 # // variables.tf
+variable "fingerprint" {
+  type        = string
+  description = "OCI API Key Fingerprint"
+}
+
+variable "private_key" {
+  type        = string
+  description = "Private Key for OCI API"
+}
 
 variable "region" {
-  description = "The OCI region to deploy to."
+  type        = string
+  description = "OCI Region"
+  default     = "ca-montreal-1"
 }
 
-variable "cidr" {
-  description = "The CIDR block for the VCN."
+variable "tenancy_ocid" {
+  type        = string
+  description = "OCI Tenancy OCID"
 }
 
-variable "azs" {
-  description = "A list of availability zones."
-  type = list(string)
+variable "user_ocid" {
+  type        = string
+  description = "OCI User OCID"
 }
-
-variable "name" {
-  description = "The name of the network."
-}
-
 variable "compartment_id" {
-  description = "The OCI compartment ID."
+  description = "The OCID of the compartment where resources will be created"
+  type        = string
+}
+
+variable "vcn_cidr" {
+  description = "The CIDR block for the VCN"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "availability_domains" {
+  description = "List of availability domains"
+  type        = list(string)
+  default     = ["AD-1", "AD-2", "AD-3"]
+}
+
+variable "subnet_cidrs" {
+  description = "Map of subnet CIDRs"
+  type        = map(string)
+  default = {
+    database    = "10.0.1.0/24"
+    private     = "10.0.2.0/24"
+    public      = "10.0.3.0/24"
+  }
 }
