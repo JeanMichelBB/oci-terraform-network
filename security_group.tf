f10ee342a747722ddc2d3127e213889c9cbe7eb5
Jean-Michel Bérubé <107640908+JeanMichelBB@users.noreply.github.com>
2025-03-09 13:06:36 -0400

    feat: ssh port (#24)

--- security_group.tf
+++ security_group.tf
@@ -1,4 +1,3 @@
-# security_group.tf
 resource "oci_core_security_list" "cluster_security_group" {
   compartment_id = var.compartment_id
   vcn_id         = oci_core_vcn.main.id
@@ -11,6 +10,20 @@ resource "oci_core_security_list" "cluster_security_group" {
         min = 80
         max = 80
     }
+  }
+
+  ingress_security_rules {
+    protocol    = "6"
+    source      = "0.0.0.0/0"
+    tcp_options {
+        min = 22
+        max = 22
+    }
+  }
+
+  ingress_security_rules {
+    protocol    = "1"  # ICMP (Ping)
+    source      = "0.0.0.0/0"
   }
 
   egress_security_rules {
