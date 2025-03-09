0cbd613ab23f606ceaf275d40becb051be022046
Jean-Michel Bérubé <107640908+JeanMichelBB@users.noreply.github.com>
2025-03-09 15:10:13 -0400

    feat: remove group (#27)

--- security_group.tf
+++ security_group.tf
@@ -14,7 +14,7 @@ resource "oci_core_security_list" "cluster_security_group" {
 
   ingress_security_rules {
     protocol    = "6"
-    source      = "your.trusted.ip.range/32"  # Restrict SSH access to trusted IPs
+    source      = "0.0.0.0/0"
     tcp_options {
         min = 22
         max = 22
@@ -34,29 +34,4 @@ resource "oci_core_security_list" "cluster_security_group" {
         max = 443
     }
   }
-}
-
-resource "oci_identity_user" "user" {
-  compartment_id = var.compartment_id
-  name           = "admin"
-  description    = "Admin User"
-}
-
-resource "oci_identity_group" "group" {
-  compartment_id = var.compartment_id
-  name           = "Administrators"
-  description    = "Administrators Group"
-}
-
-resource "oci_identity_user_group_membership" "user_group_membership" {
-  user_id  = oci_identity_user.user.id
-  group_id = oci_identity_group.group.id
-}
-
-output "user_id" {
-  value = oci_identity_user.user.id
-}
-
-output "group_id" {
-  value = oci_identity_group.group.id
 }
